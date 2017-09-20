60a3b069b36ab267d818370f19a0bd19c6a583c1
Kaleb Evans <9998226@bcs.net.nz>
2017-09-21 11:47:18 +1200

    Kaleb

---
+++ Project/main.js
@@ -0,0 +1,46 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+
+function createWindow () {
+    win = new BrowserWindow({width: 800, height: 600})
+
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+
+      win.on('closed', () => {
+      win = null
+    })
+}
+
+function ShowMe() {
+    alert("I want to see a box :-)");
+}
+
+
+//Here we call the createWindow function when the app has finished loading.
+
+app.on('ready', createWindow)
+
+//darwin is the name of the macOS kernal
+//The default behavior for a mac app is that the app is not shutdown completely when we close it.
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+//If the app becomes active, but the win variable is empty then the instruction is set to create a new window.
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
