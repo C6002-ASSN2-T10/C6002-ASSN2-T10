11fbe8cb44ad43a873fd3b7caf64cf46283dae95
kaleb evans <9998226@bcs.net.nz>
2017-10-08 21:06:16 -0700

    bug resolved

--- Project/func.js
+++ Project/func.js
@@ -1,9 +1,8 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -33.8688, lng: 151.2195},
-      zoom: 13
+      center: {lat: -37.686035, lng: 176.165824},
+      zoom: 15
     });
-    var input = document.getElementById('searchInput');
   
 
     var autocomplete = new google.maps.places.Autocomplete(input);
